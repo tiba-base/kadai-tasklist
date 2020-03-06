697488d7692c9688803768d6ada84975284b1add
tiba <tiba.contact@tiba-base.com>
2020-03-06 11:07:42 +0000

    CRUD tasks

---
+++ app/controllers/tasks_controller.rb
@@ -0,0 +1,54 @@
+class TasksController < ApplicationController
+    def index
+        @tasks = Task.all
+    end
+    
+    def show
+        @task = Task.find(params[:id])
+    end
+    
+    def new
+        @task = Task.new
+    end
+    
+    def create
+        @task = Task.new(message_params)
+        
+        if @task.save
+            flash[:success] = 'タスクが追加されました'
+            redirect_to @task
+        else
+            flash.now[:danger] = 'タスクが追加されませんでした'
+            render :new
+        end
+    end
+    
+    def edit
+       @task = Task.find(params[:id]) 
+    end
+    
+    def update
+        @task = Task.find(params[:id])
+        
+        if @task.update(message_params)
+           flash[:success] = 'タスクは正常に更新されました'
+           redirect_to @task
+        else 
+           flash.now[:danger] = 'タスクは更新されませんでした'
+           render :edit
+        end
+    end
+    
+    def destroy
+        @task = Task.find(params[:id])
+        @task.destroy
+        
+        flash[:success] = 'タスクは正常に削除されました'
+        redirect_to tasks_url
+    end
+end
+
+#Storong Parameter
+    def message_params
+        params.require(:task).permit(:content)
+    end
