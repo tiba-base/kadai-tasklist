8239e12dd26e02fda71fa1afd8270fe4764fd5dd
tiba <tiba.contact@tiba-base.com>
2020-03-07 13:39:04 +0000

    共通化

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,10 +1,11 @@
 class TasksController < ApplicationController
+    before_action :set_task, only: [:show, :edit, :update, :destroy]
+    
     def index
         @tasks = Task.all
     end
     
     def show
-        @task = Task.find(params[:id])
     end
     
     def new
@@ -12,8 +13,7 @@ class TasksController < ApplicationController
     end
     
     def create
-        @task = Task.new(message_params)
-        
+         @task = Task.new
         if @task.save
             flash[:success] = 'タスクが追加されました'
             redirect_to @task
@@ -24,11 +24,9 @@ class TasksController < ApplicationController
     end
     
     def edit
-       @task = Task.find(params[:id]) 
     end
     
     def update
-        @task = Task.find(params[:id])
         
         if @task.update(message_params)
            flash[:success] = 'タスクは正常に更新されました'
@@ -40,13 +38,17 @@ class TasksController < ApplicationController
     end
     
     def destroy
-        @task = Task.find(params[:id])
         @task.destroy
         
         flash[:success] = 'タスクは正常に削除されました'
         redirect_to tasks_url
     end
 
+private
+    
+    def set_task
+        @task =  @task = Task.find(params[:id])
+    end
 #Storong Parameter
     def message_params
         params.require(:task).permit(:content)
