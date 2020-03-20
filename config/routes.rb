15a297e9afefd556fc46c1bdb9e8a9144fb7bf27
tiba <tiba.contact@tiba-base.com>
2020-03-20 07:42:58 +0000

    user signup

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,9 @@
 Rails.application.routes.draw do
+  get 'users/show'
+  get 'users/new'
+  get 'users/create'
     root to: 'tasks#index'
     
-    resources :tasks
+    get 'signup',to: 'users#new'
+    resources :users, only: [:show, :new, :create]
 end
